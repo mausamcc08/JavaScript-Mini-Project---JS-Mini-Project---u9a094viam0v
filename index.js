6f7d0186e5523c8d96e28dbd8deedca9fcbc3c0e
mausamcc08 <92010850+mausamcc08@users.noreply.github.com>
2023-01-29 21:42:28 +0530

    Update index.js
    small changes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
+for (let i = 0; i < numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-    var buttonInnerHTML = this.innerHTML;
+    let buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
 
@@ -26,37 +26,37 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "w":
+    case "W":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
-    case "a":
+    case "A":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
-    case "s":
+    case "S":
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
 
-    case "d":
+    case "D":
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
 
-    case "j":
+    case "J":
       var snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
-    case "k":
+    case "K":
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
 
-    case "l":
+    case "L":
       var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
@@ -71,7 +71,6 @@ function makeSound(key) {
 function buttonAnimation(currentKey) {
 
   var activeButton = document.querySelector("." + currentKey);
-
   activeButton.classList.add("pressed");
 
   setTimeout(function() {
